5fe59760e4e062a96244395bfa04806cd1915dd2
chogunny1 <gunnychocho@gmail.com>
2022-10-04 00:34:44 +0900

    hp바
    hp바

--- Assets/01.Scripts/module/CharacterModule.cs
+++ Assets/01.Scripts/module/CharacterModule.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class CharacterModule : MonoBehaviour
 {
+    public Slider hpbar;
+
     public float defaultHP = 100f;
     public float defaultDF = 10f;
     public float defaultAD = 30f;
     public float defaultSpeed = 11.3f;
     public float defaultAS = 0.6f;
 
-    public float currentHP;
+    public float currentHP = 100f;
     public float currentSpeed;
 
     public bool attackAble;
@@ -30,6 +33,11 @@ public abstract class CharacterModule : MonoBehaviour
     public GameObject secondSkillEffect;
     public Transform firePos;
 
+    public void Update()
+    {
+        hpbar.value = (float)currentHP / defaultHP;
+    }
+
     public IEnumerator CharacterUpdate(float timeDelay)
     {
         StartCoroutine(KeyInPut(timeDelay));
@@ -45,7 +53,7 @@ public abstract class CharacterModule : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawCube(transform.position, new Vector3(2f,1f,1f));
+        Gizmos.DrawCube(transform.position, new Vector3(2f, 1f, 1f));
     }
 
     public void Init(float HP, float DF, float AD, float Speed, float AS)
@@ -65,9 +73,10 @@ public abstract class CharacterModule : MonoBehaviour
     public void Damage(float damage)
     {
         if (defaultDF - damage < 0)
-             currentHP -= (damage - defaultDF);
-        
+            currentHP -= (damage - defaultDF);
+
         currentHP -= 1;
+        hpbar.value = currentHP;
     }
 
     public void Attack(float AD, float AS, float Angle)
@@ -96,7 +105,7 @@ public abstract class CharacterModule : MonoBehaviour
         Destroy(this.gameObject);
     }
 
-    void OnBecameInvisible() 
+    void OnBecameInvisible()
     {
         CharacterDead();
     }
@@ -110,7 +119,7 @@ public abstract class CharacterModule : MonoBehaviour
         while (true)
         {
             moveX = 0;
-            
+
             if (Input.GetKey(KeyCode.A))
             {
                 moveX -= 1;
@@ -127,7 +136,7 @@ public abstract class CharacterModule : MonoBehaviour
             {
                 rigidbody.AddForce(Vector3.down, ForceMode2D.Impulse);
             }
-                
+
             if (Input.GetKey(KeyCode.Q))
                 FirstSkill();
 
@@ -145,7 +154,7 @@ public abstract class CharacterModule : MonoBehaviour
                 rigidbody.AddForce(Vector3.up * 6.5f, ForceMode2D.Impulse);
             }
 
-            
+
             transform.Translate(Vector3.right * moveX * currentSpeed * Time.deltaTime, Space.World);
             characterPos = transform.position;
             yield return CorutineTimeDelay;
@@ -160,7 +169,7 @@ public abstract class CharacterModule : MonoBehaviour
 
 
     public abstract void FirstSkill();
-   
+
 
     public IEnumerator FirstSkillDelayChecker(float coolTime)
     {
