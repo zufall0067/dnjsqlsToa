74457af28a9c97e3d96dc97e3d7cdd8958d3535a
chogunny1 <gunnychocho@gmail.com>
2022-10-06 20:22:28 +0900

    툴팁 수정
    툴팁 수정

--- Assets/01.Scripts/ETC/ToolTip.cs
+++ Assets/01.Scripts/ETC/ToolTip.cs
@@ -5,25 +5,39 @@ using UnityEngine;
 public class ToolTip : MonoBehaviour
 {
     public GameObject[] tooltip;
-    //public string message;
 
-    //private void OnMouseEnter()
-    //{
-    //    ToolTipManager.instance.SetAndShowToolTip(message);
-    //}
+    public void ButtonClick()
+    {
+        tooltip[0] .SetActive(true);
+    }
+    public void ButtonClickExit()
+    {
+        tooltip[0].SetActive(false);
+    }
 
-    //private void OnMouseExit()
-    //{
-    //    ToolTipManager.instance.HideToolTip();  
-    //}
+    public void ButtonClick1()
+    {
+        tooltip[1].SetActive(true);
+    }
+    public void ButtonClickExit1()
+    {
+        tooltip[1].SetActive(false);
+    }
 
-    //public void onclickbutton()
-    //{
-    //   tooltip[0].SetActive(true);
-    //}
-    //public void onclickbutton1()
-    //{
-    //    tooltip[1].SetActive(true);
-    //}
-
+    public void ButtonClick2()
+    {
+        tooltip[2].SetActive(true);
+    }
+    public void ButtonClickExit2()
+    {
+        tooltip[2].SetActive(false);
+    }
+    public void ButtonClick3()
+    {
+        tooltip[3].SetActive(true);
+    }
+    public void ButtonClickExit3()
+    {
+        tooltip[3].SetActive(false);
+    }
 }
