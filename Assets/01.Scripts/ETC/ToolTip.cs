792a3c9241b89b5db8aa39379f246dcecec4b1c7
chogunny1 <gunnychocho@gmail.com>
2022-10-06 11:02:30 +0900

    툴팁 수정 중
    툴팁 수정 중

--- Assets/01.Scripts/ETC/ToolTip.cs
+++ Assets/01.Scripts/ETC/ToolTip.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class ToolTip : MonoBehaviour
 {
-    public string message;
+    public GameObject[] tooltip;
+    //public string message;
 
-    private void OnMouseEnter()
-    {
-        ToolTipManager.instance.SetAndShowToolTip(message);
-    }
+    //private void OnMouseEnter()
+    //{
+    //    ToolTipManager.instance.SetAndShowToolTip(message);
+    //}
 
-    private void OnMouseExit()
-    {
-        ToolTipManager.instance.HideToolTip();  
-    }
+    //private void OnMouseExit()
+    //{
+    //    ToolTipManager.instance.HideToolTip();  
+    //}
+
+    //public void onclickbutton()
+    //{
+    //   tooltip[0].SetActive(true);
+    //}
+    //public void onclickbutton1()
+    //{
+    //    tooltip[1].SetActive(true);
+    //}
+
 }
